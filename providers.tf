eb53841e4f1f10930a7a140bc16c1d5f5b1c19b8
Ming Fang <mingfang@mac.com>
2024-09-01 03:28:16 +0000

    initial

---
+++ providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    coder = {
+      source = "coder/coder"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+  }
+}
+
+provider "coder" {}
+
+provider "kubernetes" {}
